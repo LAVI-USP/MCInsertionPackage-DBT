50836829e797c6e3f82ae41cd3c29219bcc0baa6
rodrigovimieiro <rodrigo.vimieiro@gmail.com>
2022-12-01 17:40:44 -0300

    code reorganization

--- main.py
+++ main.py
@@ -37,6 +37,7 @@ if __name__ == '__main__':
     
     cluster_pixel_size = 0.048                      # In mm
     detector_size = 0.1                             # In mm
+    n_max_calcs = 4
     
     
     pathPatientCases            = '/media/rodrigo/Dados_2TB/Imagens/HC_Barretos/mc_insert'
@@ -61,7 +62,7 @@ if __name__ == '__main__':
     cluster_size = [int(x/cluster_pixel_size) for x in cluster_dimensions]
     calc_window  = [int(x/cluster_pixel_size) for x in calc_dimensions]
      
-    contrasts = [0.3]
+    contrasts = [0.302]
     # for x in range(14):
     #     contrasts.append(0.85 * contrasts[x])
     
@@ -77,88 +78,88 @@ if __name__ == '__main__':
         
         for exam in exams: 
             
-            number_calc = np.random.randint(1, 7)
-
-            path2write_patient_name = "{}{}{}".format(pathPatientCalcs , filesep(), "/".join(exam.split('/')[-3:]))
-            
-            # Case already processed
-            if makedir(path2write_patient_name):
-                continue  
-
-            print("Processing " + path2write_patient_name)                   
-        
-            #%%     
-            
-            # Get  X, Y and Z position for each calcification
-            (x_calc, y_calc, z_calc), _ = get_XYZ_calc_positions(number_calc, cluster_size, calc_window, flags)
-            
-            # Load each calcification and put them on specified position
-            roi_3D, contrasts_individual = get_calc_cluster(pathCalcifications, pathCalcificationsReport, number_calc, cluster_size, x_calc, y_calc, z_calc, flags)
-            
-    
-            #%%
-            
-            dcmFiles = [str(item) for item in pathlib.Path(exam).glob("*.dcm")]
-            
-            # Run LIBRA
-            mask_dense, mask_breast, bdyThick = get_breast_masks(dcmFiles, exam, pathPatientDensity, pathLibra, pathMatlab, pathAuxLibs, flags)
-            
-            # Process dense mask
-            final_mask, flags = process_dense_mask(mask_dense, mask_breast, cluster_size, dcmFiles, flags)
-            
-            if flags['compression_paddle_found']:
-                print(exam)
-                continue
-            
-            del mask_dense, mask_breast
-            
-            #%%
-            
-            # Reconstruct the dense mask and find the coords for the cluster
-            (x_clust, y_clust, z_clust), geo, libFiles, bound_X = get_XYZ_cluster_positions(final_mask, bdyThick, pathBuildDirpyDBT, flags)
-            
-            del final_mask
-            
-            #%% 
-            
-            cluster_2D = np.sum(np.sum(roi_3D, axis=-1), axis=-1)
-            cluster_2D = 255*(cluster_2D - cluster_2D.min())/(cluster_2D.max() - cluster_2D.min())
-            
-            if flags['right_breast']:
-                x_clust2save = x_clust+bound_X
-            else:    
-                cluster_2D = np.fliplr(cluster_2D)
-                x_clust2save = geo.nx - x_clust
-                
-            plt.imsave("{}{}cluster_{}x_{}y_{}z.png".format(path2write_patient_name, filesep(), x_clust2save, y_clust, z_clust), cluster_2D, cmap='gray')
-            
-            # Inserting cluster at position and projecting the cluster mask
-            projs_masks = get_projection_cluster_mask(roi_3D, contrasts_individual, geo, x_clust, y_clust, z_clust, cluster_pixel_size, libFiles, flags)
-            
-            # Apply the fitted MTF on the mask projections
-            projs_masks_mtf = apply_mtf_mask_projs(projs_masks, len(dcmFiles), detector_size, pathMTF, flags)
-            
-            
-            cropCoords_file = pathlib.Path('{}{}{}{}Result_Images{}cropCoords.npy'.format(pathPatientDensity , filesep(), "/".join(exam.split('/')[-3:]), filesep(), filesep()))
-            if cropCoords_file.is_file():
-                cropCoords = np.load(str(cropCoords_file))
-                flags['mask_crop'] = True
-                flags['cropCoords'] = cropCoords
-            else:
-                flags['mask_crop'] = False
-                
-                
-            flags['calc_coords'] = (x_clust, y_clust, z_clust)
-            flags['bound_X'] = bound_X
-            
-            np.save(path2write_patient_name + '{}flags'.format(filesep()), flags)
-
             for contrast in contrasts:
             
+                number_calc = np.random.randint(1, n_max_calcs)
+    
+                path2write_patient_name = "{}{}{}".format(pathPatientCalcs , filesep(), "/".join(exam.split('/')[-3:]))
+                
                 path2write_contrast = "{}{}contrast_{:.3f}".format(path2write_patient_name , filesep(), contrast)
                 
-                makedir(path2write_contrast)
+                makedir(path2write_patient_name)
                 
+                # Case already processed
+                if makedir(path2write_contrast):
+                    continue                  
+    
+                print("Processing " + path2write_patient_name)                   
+            
+                #%%     
+                
+                # Get  X, Y and Z position for each calcification
+                (x_calc, y_calc, z_calc), _ = get_XYZ_calc_positions(number_calc, cluster_size, calc_window, flags)
+                
+                # Load each calcification and put them on specified position
+                roi_3D, contrasts_individual = get_calc_cluster(pathCalcifications, pathCalcificationsReport, number_calc, cluster_size, x_calc, y_calc, z_calc, flags)
+                
+        
+                #%%
+                
+                dcmFiles = [str(item) for item in pathlib.Path(exam).glob("*.dcm")]
+                
+                # Run LIBRA
+                mask_dense, mask_breast, bdyThick = get_breast_masks(dcmFiles, exam, pathPatientDensity, pathLibra, pathMatlab, pathAuxLibs, flags)
+                
+                # Process dense mask
+                final_mask, flags = process_dense_mask(mask_dense, mask_breast, cluster_size, dcmFiles, flags)
+                
+                if flags['compression_paddle_found']:
+                    print(exam)
+                    continue
+                
+                del mask_dense, mask_breast
+                
+                #%%
+                
+                # Reconstruct the dense mask and find the coords for the cluster
+                (x_clust, y_clust, z_clust), geo, libFiles, bound_X = get_XYZ_cluster_positions(final_mask, bdyThick, pathBuildDirpyDBT, flags)
+                
+                del final_mask
+                
+                #%% 
+                
+                cluster_2D = np.sum(np.sum(roi_3D, axis=-1), axis=-1)
+                cluster_2D = 255*(cluster_2D - cluster_2D.min())/(cluster_2D.max() - cluster_2D.min())
+                
+                if flags['right_breast']:
+                    x_clust2save = x_clust+bound_X
+                else:    
+                    cluster_2D = np.fliplr(cluster_2D)
+                    x_clust2save = geo.nx - x_clust
+                    
+                plt.imsave("{}{}cluster_{}x_{}y_{}z.png".format(path2write_patient_name, filesep(), x_clust2save, y_clust, z_clust), cluster_2D, cmap='gray')
+                
+                # Inserting cluster at position and projecting the cluster mask
+                projs_masks = get_projection_cluster_mask(roi_3D, contrasts_individual, geo, x_clust, y_clust, z_clust, cluster_pixel_size, libFiles, flags)
+                
+                # Apply the fitted MTF on the mask projections
+                projs_masks_mtf = apply_mtf_mask_projs(projs_masks, len(dcmFiles), detector_size, pathMTF, flags)
+                
+                
+                cropCoords_file = pathlib.Path('{}{}{}{}Result_Images{}cropCoords.npy'.format(pathPatientDensity , filesep(), "/".join(exam.split('/')[-3:]), filesep(), filesep()))
+                if cropCoords_file.is_file():
+                    cropCoords = np.load(str(cropCoords_file))
+                    flags['mask_crop'] = True
+                    flags['cropCoords'] = cropCoords
+                else:
+                    flags['mask_crop'] = False
+                    
+                    
+                flags['calc_coords'] = (x_clust, y_clust, z_clust)
+                flags['bound_X'] = bound_X
+                
+                np.save(path2write_patient_name + '{}flags_{:.3f}'.format(filesep(), contrast), flags)
+                                
                 for dcmFile in dcmFiles:
                     
                     dcmH = pydicom.dcmread(str(dcmFile))
@@ -172,7 +173,7 @@ if __name__ == '__main__':
                         dcmData = np.fliplr(dcmData)
                     
                     ind = int(str(dcmFile).split('/')[-1].split('_')[1]) - 1
-
+    
                     tmp_mask = np.abs(projs_masks_mtf[:,:,ind])
                     tmp_mask = (tmp_mask - tmp_mask.min()) / (tmp_mask.max() - tmp_mask.min())
                     tmp_mask[tmp_mask > 0] *= contrast
@@ -186,13 +187,13 @@ if __name__ == '__main__':
                     dcmFile_tmp = path2write_contrast + '{}{}'.format(filesep(), dcmFile.split('/')[-1])
                     
                     writeDicom(dcmFile_tmp, np.uint16(dcmData))
- 
+     
+                
+                
+                
+                
+                
+                    
             
             
             
-            
-            
-        
-
-
-
